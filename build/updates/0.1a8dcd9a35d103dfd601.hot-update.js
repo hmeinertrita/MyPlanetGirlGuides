5bf1177c39ab50c54f7900858f3b356c09cf9e99
hmeinertrita <hmeinertrita@gmail.com>
2017-08-24 13:16:29 -0400

    changed add button image

---
+++ build/updates/0.1a8dcd9a35d103dfd601.hot-update.js
@@ -0,0 +1,13 @@
+require("source-map-support").install();
+exports.id = 0;
+exports.modules = {
+
+/***/ "./src/routes/search/assets/add-button-small.png":
+/***/ (function(module, exports) {
+
+module.exports = "/assets/src/routes/search/assets/add-button-small.png?f8f75b05";
+
+/***/ })
+
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXBkYXRlcy8wLjFhOGRjZDlhMzVkMTAzZGZkNjAxLmhvdC11cGRhdGUuanMiLCJzb3VyY2VzIjpbIi9Vc2Vycy9obWVpbmVydHJpdGEvRG9jdW1lbnRzL015UGxhbmV0L0dpcmxHdWlkZXMvc3JjL3JvdXRlcy9zZWFyY2gvYXNzZXRzL2FkZC1idXR0b24tc21hbGwucG5nIl0sInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0gXCIvYXNzZXRzL3NyYy9yb3V0ZXMvc2VhcmNoL2Fzc2V0cy9hZGQtYnV0dG9uLXNtYWxsLnBuZz9mOGY3NWIwNVwiO1xuXG5cbi8vLy8vLy8vLy8vLy8vLy8vL1xuLy8gV0VCUEFDSyBGT09URVJcbi8vIC4vc3JjL3JvdXRlcy9zZWFyY2gvYXNzZXRzL2FkZC1idXR0b24tc21hbGwucG5nXG4vLyBtb2R1bGUgaWQgPSAuL3NyYy9yb3V0ZXMvc2VhcmNoL2Fzc2V0cy9hZGQtYnV0dG9uLXNtYWxsLnBuZ1xuLy8gbW9kdWxlIGNodW5rcyA9IDAiXSwibWFwcGluZ3MiOiI7O0E7Ozs7O0FBQUE7Ozs7QSIsInNvdXJjZVJvb3QiOiIifQ==
